a0c6b5ba7b494347703949f0f485c53434f6df60
Alexandre Rojon <alexandre.rojon@gmail.com>
2021-07-24 16:57:04 +0200

    DO NOT MERGE

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home results]
+  skip_before_action :authenticate_user!, only: %i[home results earlybird]
 
   def home
+  end
+
+  def earlybird
   end
 
   def dashboard
